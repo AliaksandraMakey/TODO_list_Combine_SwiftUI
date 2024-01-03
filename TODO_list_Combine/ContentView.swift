1a250f530ff3b76b2dde7053301c38b95d52dc94
Alexandra Makey <aliaksandra.makey@gmail.com>
2024-01-03 22:22:38 +0400

    Initial Commit

---
+++ TODO_list_Combine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TODO_list_Combine
+//
+//  Created by Александра Макей on 03.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TODO_list_Combine/TODO_list_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_list_CombineApp.swift
+//  TODO_list_Combine
+//
+//  Created by Александра Макей on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TODO_list_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
